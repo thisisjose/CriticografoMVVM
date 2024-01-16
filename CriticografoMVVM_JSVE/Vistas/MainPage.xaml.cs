45f6426a1da1b49c407b55e067e32bfd6477ec96
thisisjose <josecinuwu@gmail.com>
2024-01-15 22:25:38 -0600

    Terminado paps XD

--- CriticografoMVVM_JSVE/Vistas/MainPage.xaml.cs
+++ CriticografoMVVM_JSVE/Vistas/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CriticografoMVVM_JSVE.VistaModelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +7,17 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using CriticografoMVVM_JSVE.VistaModelo;
-using CriticografoMVVM_JSVE.Vistas;
-using CriticografoMVVM_JSVE;
-using static CriticografoMVVM_JSVE.Vistas.pagina1;
-
 
 namespace CriticografoMVVM_JSVE.Vistas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class pagina1 : ContentPage
+    public partial class MainPage : ContentPage
     {
-        public pagina1()
+        public MainPage()
         {
             InitializeComponent();
             BindingContext = new VMpagina1(Navigation);
         }
+
     }
 }
